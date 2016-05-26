806f56027db742ab54531ca4a6bce9b9ad54eaaa
ColorfulBanana <therealrebuilder@gmail.com>
2016-05-26 19:19:27 +0200

    Use bs4 again and remove use of shutil.copyfileobj
    The issue with shutil.copyfileobj is that it blocked the coroutines, since
    `resp.raw` would download it all in one go. (Don't ask me for the details, I'm
    just a programmer :p)

--- xkcd.py
+++ xkcd.py
@@ -5,13 +5,12 @@ eventlet.monkey_patch()
 
 import os
 import re
-import shutil
 
+import bs4
 import colorama
-import lxml.etree
 import requests
 
-BASE_URL = "http://xkcd.com/{}/"
+BASE_URL = "https://xkcd.com/{}/"
 FILENAME_TEMPLATE = "comics/{:04} - XKCD - {}.png"
 
 # This value limits the pool of coroutines to 255, since that's the maximum
@@ -21,43 +20,44 @@ MAXIMUM_FILE_OBJECTS = 255
 # Python 2 uses `raw_input` instead of `input`
 input_ = getattr(__builtins__, "raw_input", input)
 
+# This sets up stdout so that we can catch ANSI escape sequences (for colors)
+# and replace them with Win32 API calls.
+colorama.init()
+
+
+def cprint(color, *args, **kwargs):
+    print(color, end="")
+    print(*args, **kwargs)
+    print(colorama.Fore.RESET + colorama.Back.RESET, end="")
+
 
 def _save_comic(num):
-    # Get the so-called "soup of elements".
     response_text = requests.get(BASE_URL.format(num)).text
-    root = lxml.etree.HTML(response_text)
-
-    # Find the image url
-    comic_div = root.xpath('//div[@id="comic"]')[0]
+    soup = bs4.BeautifulSoup(response_text, "lxml")
+    comic_div = soup.find("div", id="comic")
 
     # Fixes an issue mentioned on GitHub, by only downloading comics that just
     # have the comic image in the comic div, but I am unsure of any "real"
     # comics that have more than the image.
-    if len(comic_div) != 1:
+    if len(comic_div) != 3:
         raise RuntimeError("Comic is not a regular comic!")
 
-    image_src = comic_div.find("img").get("src")
-    image_url = "http://" + image_src.replace("//", "")
-
-    # Find the official title of the comic.
-    title = root.xpath("/html/head/title/text()")[0].split("xkcd: ")[1]
-
+    image_src = comic_div.find("img")["src"]
+    image_url = "https://" + image_src.replace("//", "")
+    title = soup.find("title").text.split("xkcd: ")[1]
     # Remove any invalid characters that aren't allowed in filenames.
     title = re.sub(r"[^A-Za-z 0-9\-]", "", title)
 
     resp = requests.get(image_url, stream=True)
     filename = FILENAME_TEMPLATE.format(num, title)
-
     with open(filename, "wb") as comic_file:
-        # Write file kibibyte by kibibyte.
-        shutil.copyfileobj(resp.raw, comic_file)
-    resp.close()  # You need to close stream requests
+        for chunk in resp.iter_content():
+            comic_file.write(chunk)
+    resp.close()
 
 
 def save_comic(num):
-    print(colorama.Fore.YELLOW, end="")
-    print("Started downloading comic", num)
-    print(colorama.Fore.RESET, end="")
+    cprint(colorama.Fore.YELLOW, "Started downloading comic", num)
     try:
         _save_comic(num)
     except Exception as e:
@@ -66,7 +66,6 @@ def save_comic(num):
         return num, None
 
 def save_comics(start_num, end_num):
-    # Download all the comics asynchronously, which makes it way, WAY, faster.
     to_go = end_num + 1 - start_num
     comic_numbers = range(start_num, end_num + 1)
     p = eventlet.GreenPool(min(to_go, MAXIMUM_FILE_OBJECTS))
@@ -74,15 +73,12 @@ def save_comics(start_num, end_num):
     try:
         for (num, exception) in p.imap(save_comic, comic_numbers):
             if exception is None:
-                print(colorama.Fore.GREEN, end="")
-                print("Finished downloading comic", num)
-                print(colorama.Fore.RESET, end="")
+                cprint(colorama.Fore.GREEN, "Finished downloading comic", num)
                 successful += 1
             else:
-                print(colorama.Fore.RED, end="")
-                print("There was an error downloading comic", num, end=": ")
-                print(exception)
-                print(colorama.Fore.RESET, end="")
+                cprint(colorama.Fore.RED,
+                       "There was an error "
+                       "downloading comic {}: {}".format(num, exception))
                 failed += 1
             to_go -= 1
     except KeyboardInterrupt:
@@ -100,10 +96,8 @@ def main():
         os.mkdir("comics")
 
     if "-" in comic_input:
-        # A range was entered
         start_num, end_num = map(int, comic_input.split("-"))
     else:
-        # A single number was entered
         start_num, end_num = 1, int(comic_input)
 
     successful, failed, to_go = save_comics(start_num, end_num)
@@ -120,13 +114,10 @@ def main():
     print(msg)
     print("-" * len(msg))
 
-    print(colorama.Fore.GREEN, end="")
-    print("Successfully downloaded", successful, "comics")
-    print(colorama.Fore.RED, end="")
-    print(failed, "failed")
-    print(colorama.Fore.YELLOW, end="")
-    print(to_go, "skipped")
-    print(colorama.Fore.RESET, end="")
+    cprint(colorama.Fore.GREEN,
+           "Successfully downloaded", successful, "comics")
+    cprint(colorama.Fore.RED, failed, "failed")
+    cprint(colorama.Fore.YELLOW, to_go, "skipped")
     print("\nFiles saved in", os.path.join(os.getcwd(), "comics"))
 
 if __name__ == "__main__":
